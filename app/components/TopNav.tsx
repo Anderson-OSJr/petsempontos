8556001810b548d2243b9fd22b2a9fd0cadf7305
Anderson OSJr <anderosj@gmail.com>
2023-12-13 11:31:03 -0300

    new adjustment

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -8,16 +8,21 @@ const TopNav = () => {
             <div className="flex flex-row bg-violet-50 min-w-full
                             shadow-md shadow-violet-300">
 
-            <Image  src="/images/crochet.png"
-                              width={60}
-                              height={80}
-                              alt="Coração de Crochê"
-                              className="mx-auto my-auto"/>
+              <Image  src="/images/crochet.png"
+                                width={30}
+                                height={80}
+                                alt="Coração de Crochê"
+                                className="mx-auto my-auto"/>
 
-                <div className = "py-6 mx-auto  text-4xl text-center text-violet-700 [text-shadow:2px_2px_2px_var(--tw-shadow-color)] shadow-violet-300">
+                <div className = "py-6 mx-auto  text-5xl text-center text-violet-700 [text-shadow:2px_2px_2px_var(--tw-shadow-color)] shadow-violet-300">
                                   Pets em Pontos 
                 </div>
 
+                <Image  src="/images/crochet.png"
+                                width={30}
+                                height={80}
+                                alt="Coração de Crochê"
+                                className="mx-auto my-auto"/>
                                 
             </div>
         </div>        
