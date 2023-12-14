e8ba3c1d1b5d550fbff61ddcce6a889959d3e093
Anderson OSJr <anderosj@gmail.com>
2023-12-14 14:35:13 -0300

    Inserted images of amiguinhos

--- app/meusAmiguinhos/page.tsx
+++ app/meusAmiguinhos/page.tsx
@@ -1,7 +1,74 @@
+import Image from "next/image"
 
 const MeusAmiguinhos = () => {
   return (
-    <div>page</div>
+    <div className=" bg-violet-50 min-w-full py-8 items-center"> 
+        
+          <Image  src="/images/amiguinhos/Raposinho.png"
+                  width={240}
+                  height={300}
+                  alt="Raposinho"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/> 
+
+          <Image  src="/images/amiguinhos/porquinho.png"
+                  width={240}
+                  height={300}
+                  alt="Porquinho"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/>
+
+          <Image  src="/images/amiguinhos/sonikinho.jpg"
+                  width={240}
+                  height={300}
+                  alt="Sonikinho"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/> 
+
+          <Image  src="/images/amiguinhos/coisinha.png"
+                  width={240}
+                  height={300}
+                  alt="Porquinho"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/> 
+
+          <Image  src="/images/amiguinhos/orelhudo.png"
+                  width={240}
+                  height={300}
+                  alt="Orelhudo"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/> 
+
+          <Image  src="/images/amiguinhos/turminha.png"
+                  width={240}
+                  height={300}
+                  alt="Turminha"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/>
+
+          <Image  src="/images/amiguinhos/dinossaulo.png"
+                  width={240}
+                  height={300}
+                  alt="Dinossaulo"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/> 
+
+          <Image  src="/images/amiguinhos/coelhinhos.png"
+                  width={240}
+                  height={300}
+                  alt="Coelhinhos"
+                  className="mx-auto rounded-xl
+                            shadow-md shadow-violet-300
+                            mb-8"/>                     
+        
+      </div>   
   )
 }
 
